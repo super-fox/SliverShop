6a73c5d9f0cf5894bc8a86d8003402ed0e028511
super-fox <1396303552@qq.com>
2020-08-10 09:21:09 +0800

    主页面图片懒加载

--- src/view/home/childComps/Culture.vue
+++ src/view/home/childComps/Culture.vue
@@ -2,27 +2,24 @@
     <div class="culture">
         <h1 class="culture-text" :class="{'is_fixed':isFixed}">苗域文化</h1>
         <div class="culture-img">
-            <div class="culture-img-left">
-                <img src="~assets/img/culture/img1.jpg" alt="">
-                <img src="~assets/img/culture/img2.jpg" alt="">
-                <img src="~assets/img/culture/img3.jpg" alt="">
-                <img src="~assets/img/culture/img4.jpg" alt="">
-            </div>
-            <div class="culture-img-right">
-                <img src="~assets/img/culture/img5.jpg" alt="">
-                <img src="~assets/img/culture/img6.jpg" alt="">
-                <img src="~assets/img/culture/img7.jpg" alt="">
-                <img src="~assets/img/culture/img8.jpg" alt="">
-            </div>
+            <ul class="culture-img-left">
+                <li v-for="(item,index) in cultureImgLeft">
+                    <img v-lazy="item" alt="">
+                </li>
+            </ul>
+            <ul class="culture-img-right">
+                <li v-for="(item,index) in cultureImgRight">
+                    <img v-lazy="item" alt="">
+                </li>
+            </ul>
         </div>
         <h1 class="culture-text">制作工艺</h1>
         <div class="culture-img">
-            <div class="culture-img-left">
-                <img src="~assets/img/act/img1.jpg" alt="">
-                <img src="~assets/img/act/img2.jpg" alt="">
-                <img src="~assets/img/act/img3.jpg" alt="">
-                <img src="~assets/img/act/img4.jpg" alt="">
-            </div>
+            <ul class="culture-img-left">
+                <li v-for="(item,index) in actImg">
+                    <img v-lazy="item" alt="">
+                </li>
+            </ul>
         </div>
 
 
@@ -36,6 +33,24 @@
             return {
                 isFixed: false,
                 offsetTop: 0,
+                cultureImgLeft: [
+                    require('../../../assets/img/culture/img1.jpg'),
+                    require('../../../assets/img/culture/img2.jpg'),
+                    require('../../../assets/img/culture/img3.jpg'),
+                    require('../../../assets/img/culture/img4.jpg')
+                ],
+                cultureImgRight:[
+                    require('../../../assets/img/culture/img5.jpg'),
+                    require('../../../assets/img/culture/img6.jpg'),
+                    require('../../../assets/img/culture/img7.jpg'),
+                    require('../../../assets/img/culture/img8.jpg'),
+                ],
+                actImg:[
+                    require('../../../assets/img/act/img1.jpg'),
+                    require('../../../assets/img/act/img2.jpg'),
+                    require('../../../assets/img/act/img3.jpg'),
+                    require('../../../assets/img/act/img4.jpg')
+                ]
             }
         }
     }
@@ -56,5 +71,7 @@
         height: 160px;
         margin-left: 2px;
     }
-
+    li{
+        display: inline-block;
+    }
 </style>
